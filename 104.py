d1b362ea72427bff8f2c31b5b2ec50e60002e7f1
pohanx <14975224+veritaspoutine@users.noreply.github.com>
2020-09-03 02:03:18 +0800

    added pandas to csv feature

--- 104.py
+++ 104.py
@@ -29,6 +29,10 @@ search_results = soup.findAll('div', class_ = 'b-block__left')
 #e04 = search_results[3].text
 #print(e04)
 
+# initialize dataframe df
+columns_title = ['職位','公司', '工具', '技能', '其他']
+df = pd.DataFrame(columns=columns_title)
+
 # go through each element in search_results and use BeautifulSoup select() method to locate all a tag
 for i in search_results:
        if len(i.select('a')) == 0:
@@ -39,55 +43,45 @@ for i in search_results:
        else:
               
               job = i.select('a')[0].text
-              company = i.select('a')[1].text
+              company = i.select('a')[1].text.strip()
 
               link = i.a['href'] 
 
               link_job = link[21:26]
-              
+              ajax_link = 'https://www.104.com.tw/job/ajax/content/{}'.format(link_job)
+              ajax_headers = {
+                     'Referer':'https://www.104.com.tw/job/{}'.format(link_job),
+                     #'User-Agent' : 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0',
+              }
+              job_req = requests.get(url=ajax_link, headers = ajax_headers)
+              data = json.loads(job_req.text)['data']
+              data_condition = data['condition']
+              data_specialty = data_condition['specialty']
+
+              specialty_list = []
+              for j in data_specialty:
+                     specialty_list.append(j['description'])
+
+              skill = data_condition['skill']
+              skill_list = []
+              for i in skill:
+                     skill_list.append(i['description'])
+
+              other_list = []
+              other_list.append(data_condition['other'])
+
+              row_data = [job, company, specialty_list, skill_list, other_list]
+
+              df = df.append({'職位':row_data[0],'公司':row_data[1], '工具':row_data[2], '技能':row_data[3], '其他':row_data[4]}, ignore_index= True)
+
 
 
               #print('https:'+link)
-              #print(link_job)
-              #print(job)
-              #print(company)
+              #print(ajax_link)
+              #print(row_data)
 
-ajax_link = 'https://www.104.com.tw/job/ajax/content/703tv'
-ajax_headers = {
-       'Referer':'https://www.104.com.tw/job/703tv',
-       'User-Agent' : 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0',
-}
 
-job_req = requests.get(url=ajax_link, headers = ajax_headers)
-data = json.loads(job_req.text)['data']
-data_condition = data['condition']
+df.to_csv('./104.csv', encoding='utf-8', index=False)
 
-data_specialty = data_condition['specialty']
-print(data)
-print('--------------------')
-
-specialty_list = []
-for j in data_specialty:
-       specialty_list.append(j['description'])
-print(specialty_list)
-print('-------------')
-
-skill = data_condition['skill']
-skill_list = []
-for i in skill:
-       skill_list.append(i['description'])
-print (skill_list)
-print('--------------')
-
-print(data_condition['other'])
-other_list = []
-other_list.append(data_condition['other'])
-print(other_list)
-
-'''
-Data Frame setup
-'''
-columns_title = ['職位','公司', '工具', '技能', '其他']
-row_data = []
-df = pd.DataFrame()
+print('----------------------------')
 print(df)
